725ad8b1812fd3ebee6cd761d8c1710820bc47bb
pedrozath <pedrozath@gmail.com>
2015-05-20 12:24:12 -0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+	root to: "pages#index"
 	get "admin", to: "sessions#new"
 	delete "admin", to: "sessions#destroy"
 	get "/", to: "events#graphic_design", constraints: {subdomain: "graphicdesign"}
 	resources :sessions, path: "admin"
-	resources :events, path: "/" do 
+	resources :events, path: "biography" do 
 		resources :files
 	end
 end
