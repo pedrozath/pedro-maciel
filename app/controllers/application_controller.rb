64a3705a7910e479ac3158487a1d96581ad3e0a2
Pedro Maciel <pedrozath@gmail.com>
2016-02-17 17:59:04 -0200

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,3 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
-
-	before_action :apply_locale
-
-	def apply_locale
-		set_locale if location_not_set?
-		I18n.locale = session[:locale] || :en
-	end
-
-	def set_locale
-		session[:locale] = case request.location.country_code
-			when "BR" then :pt_BR
-			when "PT" then :pt_BR
-			else "en"
-		end
-	end
-
-	def location_not_set?
-		!session.include? :locale
-	end
 end
