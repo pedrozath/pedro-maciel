8c0ff15ed0b623274b70a3085696c9fa7399b374
Pedro Maciel <pedrozath@gmail.com>
2016-06-02 15:29:53 -0300

    fix

--- app/helpers/content_sections_helper.rb
+++ app/helpers/content_sections_helper.rb
@@ -1,5 +1,5 @@
 module ContentSectionsHelper
     def render_technologies section
-        render partial: "content_sections/technologies", locals: {technologies: section.technologies}
+        render partial: "content_sections/technologies", locals: {technologies: section.technologies} unless section.technologies.nil?
     end
 end
