4d46dd4d06e8adc72c38709564b18c2a5c278b31
Pedro Maciel <pedrozath@gmail.com>
2017-11-21 11:10:54 -0200

    updating beta

--- beta/index.js
+++ beta/index.js
@@ -1,4 +1,4 @@
 (function() {
-  $("main").html("<div class='monitor aspect-ratio'><div class='work video'></div></div><nav class='main'><a href='javascript:void(0)'>Watch my introduction</a><a data-auto-route href='portfolio'>See my portfolio</a><!-- %a{:href => '//br.linkedin.com/pub/pedro-maciel/22/752/a41', target: '_blank'} LinkedIn --></nav>");
+  $("main").html("<div class='monitor aspect-ratio'><div class='work video'></div></div><nav class='main'><a class='watch-introduction' href='javascript:void(0)'>Watch my introduction</a><a data-auto-route href='portfolio'>See my portfolio</a><!-- %a{:href => '//br.linkedin.com/pub/pedro-maciel/22/752/a41', target: '_blank'} LinkedIn --></nav>");
 
 }).call(this);
