946ad137dcddba9776d922f0998a7b549f434100
Pedro Maciel <pedrozath@gmail.com>
2017-12-12 09:03:41 -0200

    updates site

--- Gemfile
+++ Gemfile
@@ -4,12 +4,14 @@ gem 'jekyll'
 gem 'activesupport'
 gem 'pry'
 gem 'mini_magick'
+gem 'font-awesome-sass'
+gem 'image_optim'
 
 group :jekyll_plugins do
   gem 'jekyll-assets'
   gem 'jekyll-coffeescript'
   gem 'jekyll-livereload'
-  gem 'jekyll-haml', path: '../jekyll-haml'
+  gem 'jekyll-haml', github: 'pedrozath/jekyll-haml'
   gem 'jekyll-feed', '~> 0.6'
 end
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 (function() {
-  $("main").html("<div class='contact-info'><div class='embedded-video'><iframe allowfullscreen class='motion-reel' frameborder='0' mozallowfullscreen src='https://player.vimeo.com/video/146427359?color=fdf16f&amp;byline=0&amp;portrait=0' webkitallowfullscreen></iframe></div><hr><h2 class='h2'>PEDRO@PEDROMACIEL.COM / +55 11 95252 8011</h2><ul class='inline'><li><a data-auto-route href='portfolio'>portfolio</a></li><li><a href='//br.linkedin.com/pub/pedro-maciel/22/752/a41' target='_blank'>LinkedIn</a></li></ul></div>");
+  $("main").html("<div class='monitor aspect-ratio'><div class='work video'><video autoplay class='screen' data-src='/assets/screen-ab799b45adcb21821e13e300c2462b4eb8c91bcde536b4ee58b53fb4712ed1b0.mp4' loop mute></video></div></div><nav class='main'><a class='watch-introduction disabled' href='javascript:void(0)'>Loading my introduction…</a><a data-auto-route href='portfolio'>See my portfolio</a><!-- %a{:href => '//br.linkedin.com/pub/pedro-maciel/22/752/a41', target: '_blank'} LinkedIn --></nav>");
 
 }).call(this);
